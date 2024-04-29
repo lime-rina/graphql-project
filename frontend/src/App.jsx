fecee608217a697247d4d2e7f56aa2e181d4f22b
lime-rina <marinayordanova@limechain.tech>
2024-04-29 10:26:44 +0300

    UI updated - all needed components are added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,23 @@
+import HomePage from "./pages/Home";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import TransactionPage from "./pages/TransactionPage";
+import NotFound from "./pages/NotFound";
+import Header from "./components/UI/Header";
+import { Routes, Route } from "react-router-dom";
 function App() {
+  const authUser = true;
   return (
     <>
-      <h1 className="text-red-500"> Hello</h1>
+      {authUser && <Header />}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/transaction/:id" element={<TransactionPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
   );
 }
-
 export default App;
